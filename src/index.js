bd68deeb443640cf1027363227092c2f20b1eb87
1481750684 <1481750684@qq.com>
2019-09-10 19:59:33 +0800

    init admin

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'antd/dist/antd.css' // 引入antd的全局样式文件
+import axios from 'utils/axios.js'
+
+React.Component.prototype.$axios = axios
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
